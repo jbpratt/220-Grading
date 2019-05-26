036250a873c8eb0d198b67331f0e49359e22e8d3
jbpratt78 <jbpratt78@gmail.com>
2019-05-26 12:37:18 -0500

    start of c# solution

---
+++ c#-assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c__assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
